484cd2bdfc9fad3c6493bab66f56796cdd7643e9
banaprojects <cyzterance06@gmail.com>
2024-08-13 11:06:40 +0200

    fixed dowloading error

--- main.py
+++ main.py
@@ -2,19 +2,25 @@
 import yt_dlp 
 import tkinter as tk
 import customtkinter as ctk
+import os
 
 #DOWNLOAD FUNCTION
 def download_video():
     try:
         ydl_opts = {
-            'format': 'bestaudio/best',
+            'format': 'best',
             'outtmpl': '%(title)s.%(ext)s',
-            # 'postprocessors': [{
-            #     'key': 'FFmpegExtractAudio',
-            #     'preferredcodec': 'mp3',
-            #     'preferredquality': '192',
-            # }],
-            # 'cookiesfrombrowser': 'chrome',
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([URL.get()])
+    except Exception as e:
+        print(e)
+
+def download_audio():
+    try:
+        ydl_opts = {
+            'format': 'bestaudio',
+            'outtmpl': '%(title)s.%(ext)s',
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([URL.get()])
@@ -27,7 +33,7 @@ ctk.set_default_color_theme("blue")
 
 #UI SETUP
 app = ctk.CTk()
-app.geometry("720x480")
+app.geometry("720x280")
 app.title("Youtube Downloader")
 
 #UI ELEMENTS
@@ -39,9 +45,10 @@ link = ctk.CTkEntry(app, placeholder_text="Enter URL", width=350, height=40, tex
 link.pack()
 
 #Download Btn
-download = ctk.CTkButton(app, text="Download", command=download_video)
+download = ctk.CTkButton(app, text="Download Video", command=download_video)
 download.pack(padx=10, pady=10)
-
+download = ctk.CTkButton(app, text="Downoad Audio", command=download_audio)
+download.pack(padx=10, pady=10)
 
 
 #RUN APP
